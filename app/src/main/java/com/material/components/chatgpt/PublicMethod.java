e1359f2f31b5448aa8de572c4c081a1df5217417
LiYaokun97 <nukoayil@qq.com>
2023-03-18 11:37:41 +0100

    [chat-gpt] set timeout=30s

--- app/src/main/java/com/material/components/chatgpt/PublicMethod.java
+++ app/src/main/java/com/material/components/chatgpt/PublicMethod.java
@@ -1,15 +1,20 @@
 package com.material.components.chatgpt;
 
+import android.os.Build;
+
 import com.theokanning.openai.service.OpenAiService;
+
+import java.time.Duration;
 
 public class PublicMethod {
     private static final String token = "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";
     public static OpenAiService service;
     public static OpenAiService getOpenAiService(){
         if (service == null) {
-            service = new OpenAiService(token);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                service = new OpenAiService(token, Duration.ofSeconds(30));
+            }
         }
         return service;
     }
-
 }
