36981a98cebef524e830e9744229ee00552718fe
LiYaokun97 <nukoayil@qq.com>
2023-04-11 16:48:10 +0200

    [chat-gpt] 暂存

--- chatgpt/src/main/java/com/example/chatgpt/fragment/ChatGptMeFragment.java
+++ chatgpt/src/main/java/com/example/chatgpt/fragment/ChatGptMeFragment.java
@@ -1,17 +1,25 @@
 package com.example.chatgpt.fragment;
 
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.Fragment;
 
+import com.blankj.utilcode.util.ActivityUtils;
 import com.example.chatgpt.R;
+import com.example.chatgpt.common.XLIntent;
+import com.google.android.gms.samples.wallet.activity.CheckoutActivity;
 
 
 public class ChatGptMeFragment extends Fragment {
+
+    private RelativeLayout payButton;
+
 
     public ChatGptMeFragment() {
     }
@@ -19,10 +27,21 @@ public class ChatGptMeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_setting, container, false);
-
+        payButton = root.findViewById(R.id.setting_pay_button);
+        initView();
         return root;
     }
 
+    public void initView() {
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new XLIntent(ActivityUtils.getTopActivity(), CheckoutActivity.class);
+                ActivityUtils.getTopActivity().startActivity(intent);
+            }
+        });
+
+    }
 
 
 }
