da8909eb643d8d00fbf4a4b78b331c06d0e580b0
LiYaokun97 <nukoayil@qq.com>
2023-05-25 02:12:56 +0200

    [fix] ReviewCardView 还未实现自适应字体大小

--- chatgpt/src/main/java/com/chunxia/chatgpt/adapter/review/ReviewCardView.java
+++ chatgpt/src/main/java/com/chunxia/chatgpt/adapter/review/ReviewCardView.java
@@ -6,10 +6,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,6 +24,8 @@ public class ReviewCardView extends LinearLayout {
     private TextView mLTextView;
     private TextView tLTextView;
     private float defaultTargetTextSize;
+    private float tLTextSize;
+    private float mLTextSize;
     private int topMargin, middleMargin, bottomMargin;
 
     public ReviewCardView(Context context, AttributeSet attrs) {
@@ -34,8 +40,8 @@ public class ReviewCardView extends LinearLayout {
         int tLTextStyle = typedArray.getInt(R.styleable.ReviewCardView_tLTextStyle, Typeface.NORMAL);
         int mLTextColor = typedArray.getColor(R.styleable.ReviewCardView_mLTextColor, Color.BLACK);
         int tLTextColor = typedArray.getColor(R.styleable.ReviewCardView_tLTextColor, Color.BLACK);
-        float mLTextSize = typedArray.getDimension(R.styleable.ReviewCardView_mLTextSize, dpToPx(context, 10));
-        float tLTextSize = typedArray.getDimension(R.styleable.ReviewCardView_tLTextSize, dpToPx(context, 13));
+        mLTextSize = typedArray.getDimension(R.styleable.ReviewCardView_mLTextSize, dpToPx(context, 20));
+        tLTextSize = typedArray.getDimension(R.styleable.ReviewCardView_tLTextSize, dpToPx(context, 22));
         typedArray.recycle();
 
         // 初始化视图，如创建中英文TextView等
@@ -73,34 +79,71 @@ public class ReviewCardView extends LinearLayout {
         defaultTargetTextSize = tLTextView.getTextSize();
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//
+//        int totalHeight = getMeasuredHeight();
+//        float targetHeight = totalHeight * 0.6f;
+//
+//        Paint mLanguagePaint = mLTextView.getPaint();
+//        Paint tLanguagePaint = tLTextView.getPaint();
+//
+//        float mLTextHeight = mLanguagePaint.descent() - mLanguagePaint.ascent();
+//        float tLTextHeight = tLanguagePaint.descent() - tLanguagePaint.ascent();
+//
+//        float totalTextHeight = mLTextHeight + tLTextHeight + middleMargin;
+//
+//        if (totalTextHeight > targetHeight) {
+//            float scale = targetHeight / totalTextHeight;
+//            float newtLTextSize = defaultTargetTextSize * scale;
+//            tLTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, newtLTextSize);
+//        } else {
+//            tLTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, defaultTargetTextSize);
+//        }
+//
+//        requestLayout();
+//        // 再次调用super.onMeasure以使用新的文本大小
+//        // super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//    }
 
-        int totalHeight = getMeasuredHeight();
-        float targetHeight = totalHeight * 0.6f;
-
-        Paint mLanguagePaint = mLTextView.getPaint();
-        Paint tLanguagePaint = tLTextView.getPaint();
-
-        float mLTextHeight = mLanguagePaint.descent() - mLanguagePaint.ascent();
-        float tLTextHeight = tLanguagePaint.descent() - tLanguagePaint.ascent();
-
-        float totalTextHeight = mLTextHeight + tLTextHeight + middleMargin;
-
-        if (totalTextHeight > targetHeight) {
-            float scale = targetHeight / totalTextHeight;
-            float newtLTextSize = defaultTargetTextSize * scale;
-            tLTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, newtLTextSize);
-        } else {
-            tLTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, defaultTargetTextSize);
-        }
-
-        requestLayout();
-        // 再次调用super.onMeasure以使用新的文本大小
-        // super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        Log.i("ReviewCardView", "onMeasure");
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        int full = getMeasuredHeight();
+//        float targetHeight = full * 0.6f;
+//        float height1 = measureTextViewHeight(mLTextView, mLTextSize);
+//        float height2 = measureTextViewHeight(tLTextView, tLTextSize);
+//        Log.i("ReviewCardView", "tLTextView height: " + height2 + ", size:" + tLTextSize);
+//        float currentHeight = height1 + height2 + middleMargin;
+//
+//        float newtLTextSize = tLTextSize;
+//        while (currentHeight > targetHeight) {
+//            Log.i("ReviewCardView", "currentHeight:" + currentHeight + ", targetHeight:" + targetHeight);
+//            newtLTextSize = newtLTextSize - 3;
+//            tLTextSize = newtLTextSize;
+//            height1 = measureTextViewHeight(mLTextView, mLTextSize);
+//            height2 = measureTextViewHeight(tLTextView, tLTextSize);
+//            Log.i("ReviewCardView", "after change tLTextView height: " + height2 + ", size:" + newtLTextSize);
+//            currentHeight = height1 + height2 + middleMargin;
+//        }
+//        if (newtLTextSize != tLTextSize){
+//            tLTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, newtLTextSize);
+//        }
+//
+////         再次调用super.onMeasure以使用新的文本大小
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//    }
+//
+//    private float measureTextViewHeight(TextView textView, float textSize) {
+//        Paint paint = textView.getPaint();
+//        paint.setTextSize(textSize);//设置字体大小
+//        float oneLine = paint.descent() - paint.ascent();
+//        int lineNumber = textView.getLineCount();
+//        float space = textView.getLineSpacingExtra();
+//        return oneLine * lineNumber + space * (lineNumber - 1);
+//    }
 
     public void setSentence(String s) {
         tLTextView.setText(s);
